b16ef12e0964c39aad3a1acc7f826431074a2c0d
RezaMaulana <reza.maulana.tif422@polban.ac.id>
2025-02-20 11:44:20 +0700

    update progress

--- orderservice/src/main/java/com/zeuss/orderservice/service/OrderService.java
+++ orderservice/src/main/java/com/zeuss/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.zeuss.orderservice.service;
 
+import com.zeuss.orderservice.client.InventoryClient;
 import com.zeuss.orderservice.dto.OrderRequest;
 import com.zeuss.orderservice.model.Order;
 import com.zeuss.orderservice.repository.OrderRepository;
@@ -13,13 +14,20 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);
+        }else {
+            throw new RuntimeException("Product with skuCode" + orderRequest.skuCode() + " is not in stock");
+        }
     }
 }
